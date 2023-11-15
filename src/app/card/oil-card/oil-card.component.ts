6d8866b208c4f54588cc32c15846bf6099e77e7c
Batischev <batischev@playkot.com>
2023-11-15 18:09:22 +0300

    обновление

--- src/app/card/oil-card/oil-card.component.ts
+++ src/app/card/oil-card/oil-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { OilCard } from 'src/app/models/oil-card.model';
 
 @Component({
@@ -8,4 +8,14 @@ import { OilCard } from 'src/app/models/oil-card.model';
 })
 export class OilCardComponent {
   @Input() item: OilCard = <OilCard>{};
+
+  public defaultPicture: string = 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+
+  public getPictureUrl(pictureUrl: string): string {
+    if (pictureUrl != '') {
+      console.log(pictureUrl)
+      return pictureUrl;
+    }
+    return this.defaultPicture;
+  }
 }
--- src/app/models/oil-card.model.ts
+++ src/app/models/oil-card.model.ts
@@ -6,4 +6,5 @@ export interface OilCard {
     actualCount: number;
     brand: string;
     url: string;
+    picture: string;
 }
